4a5cb4376c17dc81a51f26169542dd60d4f21d0b
sleepyshowdy <hojo.miku@gmail.com>
2022-12-11 01:21:51 +1100

    ReadContinue修正

--- src/components/ReadContinue.tsx
+++ src/components/ReadContinue.tsx
@@ -1,4 +1,4 @@
-import { Text } from "native-base";
+import { Text, View } from "native-base";
 //npm i --save-dev @types/react-native-read-more-textをしたらエラー解決した
 
 import ReadMore from "react-native-read-more-text";
@@ -8,17 +8,17 @@ import basicStyles from "./styles/theme/basicStyleSheet";
 export const ReadContinue = ({ section }) => {
   const _renderTruncatedFooter = (handlePress: any) => {
     return (
-      <Text color="#76785F" marginTop={5} onPress={handlePress}>
-        もっと読む
-      </Text>
+      <View mt="5" color="#76785F">
+        <Text onPress={handlePress}>もっと読む</Text>
+      </View>
     );
   };
 
   const _renderRevealedFooter = (handlePress: any) => {
     return (
-      <Text color="#76785F" marginTop={5}>
-        たたむ
-      </Text>
+      <View mt="5" color="#76785F">
+        <Text onPress={handlePress}>たたむ</Text>
+      </View>
     );
   };
 
